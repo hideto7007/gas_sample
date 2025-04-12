b1c3d8b98d7d0375737cf7bdc72aea19829fc58e
hideto <ma_kux@icloud.com>
2025-04-12 23:56:00 +0900

    関数追加

--- src/main/sample.js
+++ src/main/sample.js
@@ -2,7 +2,18 @@ function sample(value) {
   return value === "sample" ? "ok" : "ng";
 }
 
+const getInfo = (data) => {
+  if (data.num * 2 > 26) {
+    data.result = "ok"
+  } else if (data.num * 2 < 20) {
+    data.result = "good"
+  } else {
+    data.result = "No"
+  }
+  return data
+}
+
 // Node.js 環境なら export する（GASでは無視される）
 if (typeof module !== "undefined") {
-  module.exports = { sample };
+  module.exports = { sample, getInfo };
 }
--- src/test/main/sample.test.js
+++ src/test/main/sample.test.js
@@ -1,4 +1,4 @@
-const { sample } = require("../../main/sample");
+const { sample, getInfo } = require("../../main/sample");
 
 describe("sample", () => {
   test("sample success return OK", () => {
@@ -7,5 +7,34 @@ describe("sample", () => {
 
   test("sample success return NG", () => {
     expect(sample("test")).toBe("ng");
+  });
+});
+
+describe("getInfo", () => {
+  test("getInfo success return ok", () => {
+    expect(
+      getInfo({
+        num: 13.1,
+        result: "",
+      })
+    ).toEqual({ num: 13.1, result: "ok" });
+  });
+
+  test("getInfo success return good", () => {
+    expect(
+      getInfo({
+        num: 9,
+        result: "",
+      })
+    ).toEqual({ num: 9, result: "good" });
+  });
+
+  test("getInfo success return No", () => {
+    expect(
+      getInfo({
+        num: 10,
+        result: "",
+      })
+    ).toEqual({ num: 10, result: "No" });
   });
 });
